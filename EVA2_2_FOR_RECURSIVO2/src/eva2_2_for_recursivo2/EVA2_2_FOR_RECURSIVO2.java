c4321d845d40bf5b243f1927ca2c2c3820f1a992
GardeaBeltran <93112421+GardeaBeltran@users.noreply.github.com>
2021-11-04 18:28:06 -0700

    Add files via upload

---
+++ EVA2_2_FOR_RECURSIVO2/src/eva2_2_for_recursivo2/EVA2_2_FOR_RECURSIVO2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_2_for_recursivo2;
+
+/**
+ *
+ * @author Ximena Hernandez
+ */
+public class EVA2_2_FOR_RECURSIVO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FakeForDown(5);
+    }
+    
+    public static void FakeForDown(int iVal){
+        System.out.print(iVal + "-");
+        if (iVal>1) {
+         
+        FakeForDown(iVal - 1);
+        } 
+    }
+}
