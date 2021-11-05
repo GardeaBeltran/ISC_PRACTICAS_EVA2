ab59d9cea5545de3397cdd2b33fa26ba9ebaf898
GardeaBeltran <93112421+GardeaBeltran@users.noreply.github.com>
2021-11-04 18:27:26 -0700

    Add files via upload

---
+++ EVA2_1_FOR_RECURSIVE/src/eva2_1_for_recursive/EVA2_1_FOR_RECURSIVE.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_1_for_recursive;
+
+/**
+ *
+ * @author Ximena Hernandez
+ */
+public class EVA2_1_FOR_RECURSIVE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        recursivo();
+    }
+    
+    public static void recursivo(){
+        System.out.println("HOLS MUNDO!!");
+        recursivo();
+    } 
+}
